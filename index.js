159532d425d01b0cdfa5c882ee45b7aabfaa7988
yalper55 <quadmxteam@hotmail.com>
2020-08-24 22:23:31 -0300

    test2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console console.log();
+let pepe = 'hello cold world'
+console.log('really '+ pepe)
+
+const pepeobject = {
+    usuario: 'soy pepe',
+    contraseña: 1235,
+    p:true
+}
+console.log(pepeobject) 
