acb6f440c63513cd249e4b092229763dd811f235
yalper55 <quadmxteam@hotmail.com>
2020-08-27 19:37:35 -0300

    tesst

--- todolist/app.js
+++ todolist/app.js
@@ -1,6 +1,28 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];//guarda localmente los datos (json.parce para q lea datos ademas de los string)
+
+
+const render = () => {
+    const todolist = document.getElementById('todo-list');
+    const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+    todolist.innerHTML = todosTemplate.join('');
+    const elementos = document.querySelectorAll('#todo-list li')
+    elementos.forEach((elemento, i) => {
+    elemento.addEventListener('click', () => {
+        elemento.parentNode.removeChild(elemento) // usa el nodo padre para eliminar a sus hijos (elimina con click dato ingresado)
+        todos.splice(i,1) //indice q queresmos eliminar y la cantidad (limpia variable todos)
+        actualizaTodos(todos)
+        render()
+    })
+})
+}
+const actualizaTodos = (todos) => {
+    const todoStrings = JSON.stringify(todos) //lo pasa a un string
+        localStorage.setItem('todos',todoStrings)
+
+}
 
 window.onload = () => {
+    render()
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) => {
         e.preventDefault(); //previene que se actualice la pagina
@@ -8,16 +30,8 @@ window.onload = () => {
         const todoText = todo.value;
         todo.value = '';
         todos.push(todoText);
-        const todolist = document.getElementById('todo-list');
-        const todosTemplate = todos.map(t => '<li>' + t + '</li>');
-        todolist.innerHTML = todosTemplate.join('');
-        const elementos = document.querySelectorAll('#todo-list li')
-        elementos.forEach((elemento, i) => {
-            elemento.addEventListener('click', () => {
-                console.log(elemento, i);
-  
-            })
-        })
+        actualizaTodos(todos)
+        render()      
     }
 }
 
