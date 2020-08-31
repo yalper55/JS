f744ba5f22790c1834810ebace372a5288580378
yalper55 <quadmxteam@hotmail.com>
2020-08-31 18:14:51 -0300

    t

--- intro-node/index.js
+++ intro-node/index.js
@@ -1,1 +1,9 @@
 const express = require('express')
+const app = express()
+
+app.get('*', (request, response) => {
+    response.send({ message: 'chanchito feliz'})
+})
+
+app.listen(3000, () => console.log('our server is waiting for port 3000'))
+
