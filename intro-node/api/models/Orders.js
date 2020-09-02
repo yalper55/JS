06ed0d9d431b16c0fb53410914c7a9162d100b2e
yalper55 <quadmxteam@hotmail.com>
2020-09-02 14:26:50 -0300

    dinnertime

---
+++ intro-node/api/models/Orders.js
@@ -0,0 +1,11 @@
+const { Mongoose } = require("mongoose")
+
+Const mongoose = require ('mongoose')
+const Schema = mongoose.Schema
+
+const Orders = mongoose.model('Order',new Schema({
+    meal_id: { type: Schema.Types.ObjectId, ref: 'Meal'},
+    user_id: String,
+}))
+
+module.exports = Orders
