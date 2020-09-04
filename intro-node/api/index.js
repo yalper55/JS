ad5477be7f925b7ce97e71c44e719e7ebeec4c1c
yalper55 <quadmxteam@hotmail.com>
2020-09-04 15:24:55 -0300

    3

--- intro-node/api/index.js
+++ intro-node/api/index.js
@@ -5,6 +5,7 @@ const cors = require ('cors')
 
 const meals = require('./routes/meals')
 const orders = require('./routes/orders')
+
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
--- intro-node/api/models/Meals.js
+++ intro-node/api/models/Meals.js
@@ -1,4 +1,4 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Meals = mongoose.model('Meal', new Schema({
